5b3029f59ef02369d6458794c7469367767ff2ea
mac <mac@MacBookPro.lan>
2025-04-13 10:34:35 +0530

    Callbacks

---
+++ lec10/5_callbacks/2_callBackExample.js
@@ -0,0 +1,28 @@
+function f1(cb){
+    setTimeout(()=>{
+        console.log("Maggi aa gyi ghar pe!")
+        cb()
+    },5000)
+}
+
+function f2(cb){
+    setTimeout(()=>{
+        console.log("Maggi ban gyi")
+        cb()
+    }, 4000)
+}
+
+function f3(){
+    setTimeout(()=>{
+        console.log("Maggi serve ki gyi")
+    }, 4000)
+}
+
+
+f1(()=>{
+    f2(()=>{
+        f3()
+    })
+})
+
+
