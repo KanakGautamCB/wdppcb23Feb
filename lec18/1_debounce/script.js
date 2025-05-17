152329995e9ba458b3f5bb6d802f03ba6ec5f2d4
mac <mac@MacBookPro.lan>
2025-05-17 12:31:55 +0530

    lec18

---
+++ lec18/1_debounce/script.js
@@ -0,0 +1,20 @@
+
+function debounce(cb){
+    let timer;
+    return function(){
+        clearTimeout(timer);
+
+        timer = setTimeout(()=>{
+            cb()
+        },2000)
+    }
+}
+
+const inputBox = document.querySelector('#input-box').addEventListener('keydown',debounce(fun))
+
+function fun(){
+    console.log(document.querySelector('#input-box').value)
+}
+
+
+
